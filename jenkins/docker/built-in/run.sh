fc15e31dc80983a0fcff740a1aed81f1a9f92ed0
Alexey Drozdov <daju@yandex.ru>
2025-06-13 14:52:54 +0000

    built-in Dockerfile and run.sh update

--- jenkins/docker/built-in/run.sh
+++ jenkins/docker/built-in/run.sh
@@ -1,5 +1,8 @@
 # https://www.jenkins.io/blog/2023/03/27/repository-signing-keys-changing/
 
+PROJECTS_DIR=$(dirname $(dirname ${PWD}))
+USR3_DIR=$(dirname $(dirname $(dirname $(dirname ${PWD}))))
+
 docker run \
   --name jenkins-blueocean \
   --restart=on-failure \
@@ -10,6 +13,8 @@ docker run \
   --env DOCKER_TLS_VERIFY=1 \
   --publish 8080:8080 \
   --publish 50000:50000 \
+  --volume ${PROJECTS_DIR}:${PROJECTS_DIR} \
+  --volume ${USR3_DIR}/CI_CD:${USR3_DIR}/CI_CD \
   --volume /home/${USER}/jenkins:/home/${USER}/jenkins \
   --volume jenkins-data:/var/jenkins_home \
   --volume jenkins-docker-certs:/certs/client:ro \
