9d0f9b23f63c7c8749bd6b0f8dbc44f624860ece
Alexey Drozdov <daju@yandex.ru>
2025-06-13 15:16:12 +0000

    git-server/ubivisgmbh/run.sh update

--- jenkins/docker/git-server/ubivisgmbh/run.sh
+++ jenkins/docker/git-server/ubivisgmbh/run.sh
@@ -1,8 +1,9 @@
-PROJECT_ROOT=$(dirname ${PWD})
+USR3_ROOT=$(dirname $(dirname $(dirname $(dirname $(dirname ${PWD})))))
+PROJECT_ROOT=$(dirname$(dirname ${PWD}))
 
 docker run -d -p 2022:22 \
 	--name ubivisgmbh_git_server \
-	-v ${PROJECT_ROOT}/gitserver:/srv/git \
+	-v ${USR3_ROOT}/CI_CD/gitserver:/srv/git \
  	-e GIT_AUTHORIZED_KEYS="$(cat ~/.ssh/id_rsa.pub)" \
 	-e GIT_HOST_HINT="example.org:2022" \
 	--network jenkins \
